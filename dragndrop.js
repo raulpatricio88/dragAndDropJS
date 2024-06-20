bae3e925ce9dd3fdef129b1f1040a10f9401b240
Re Doble <raulpatricio88@gmail.com>
2024-06-20 00:00:22 -0400

    Drag And Drop OK

--- dragndrop.js
+++ dragndrop.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', iniciarApp );
 
 function iniciarApp() {
 
-    var contenedor = document.querySelector('#contenedor');
-    var narrowDiv = document.querySelector('#narrow');
+    var contenedor = document.querySelector('#contenedor'); // contenedor derecho
+    var narrowDiv = document.querySelector('#narrow'); // contenedor izquierdo
+    const totalDiv = document.querySelector('#total');
+
     
 
     obtenerDatos();
@@ -18,61 +20,102 @@ function obtenerDatos() {
 function cargarImgNarrow(imagenes = []) {
     
     imagenes.forEach( img => {
+        
         const { imagen, precio, nombre, id } = img;
         var productoImg = document.createElement('IMG');
         productoImg.src = imagen;
-        productoImg.precio = precio;
-        productoImg.nombre = nombre;
+        productoImg.value = precio;
+        productoImg.name = nombre;
         productoImg.id = id;
         productoImg.alt = "imagen";
         productoImg.draggable = true;
         productoImg.classList.add('draggable', 'drag-item');
+        productoImg.style.cssText = 'width: 100px; height: 100px; margin-bottom: 5px; padding: 10px 10px; border-radius: 20px;';
+        productoImg.addEventListener("dragstart", event => dragStart(event));
         narrowDiv.appendChild(productoImg);
         
     })
 }
-    
-var draggableElements = document.querySelectorAll(".draggable");
 
-
-draggableElements.forEach(elem => {
-    elem.addEventListener("dragstart", dragStart);
-})
 
 // drag andr drop functions
+// eventos
+contenedor.addEventListener('dragover', dragOver);
+contenedor.addEventListener('drop', drop); 
+narrowDiv.addEventListener('drop', drop2); 
+narrowDiv.addEventListener('dragover', dragOver); 
 
 function dragStart(event) {
     
-    console.log("Arrastrando...")
+    //console.log("Dragging...")
     event.dataTransfer.setData("text/plain", event.target.id);
    // event.dataTransfer.setData("text", event.target.value);
 }
 
-contenedor.addEventListener('dragover', dragOver);
+
 
 function dragOver(event) {
     event.preventDefault();
+    //console.log("Dragover...")
 }
-//contenedor.addEventListener('drop', drop); 
 
-// function drop(event) {
 
-//     // setear la imagen en el drop
-//     event.preventDefault();
-//     const draggableElementData = event.dataTransfer.getData("text");
-//     const draggableElement = document.getElementById(draggableElementData);
-//     // event.target.insertAdjacentHTML("afterbegin", `<img id="${draggableElementData}" class="draggable" draggable="true" src="img/1.jpeg" alt="Imagen 1">`)
-//     draggableElement.style.cssText = 'max-width: 100px; max-height: 100px; margin-bottom: 20px; padding: 10px 20px;';
-//     contenedor.appendChild(draggableElement);
+function drop(event) {
+
+    // setear la imagen en el drop
+    event.preventDefault();
+    const draggableElementData = event.dataTransfer.getData("text/plain");
+    const draggableElement = document.getElementById(draggableElementData);
+    // event.target.insertAdjacentHTML("afterbegin", `<img id="${draggableElementData}" class="draggable" draggable="true" src="img/1.jpeg" alt="Imagen 1">`)
+    //draggableElement.style.cssText = 'max-width: 100px; max-height: 100px; margin-bottom: 20px; padding: 10px 20px; border-radius: 20px;';
+    draggableElement.style.cssText = 'width: 100px; height: 100px; margin-bottom: 5px; padding: 10px 10px; border-radius: 20px;';
+    contenedor.appendChild(draggableElement);
+    calcularTotal(draggableElement.value);
     //console.log(draggableElementData); // muestra el id de la imagen el nombre
 
+}
+
+function drop2(event) {
+    // setear la imagen en el drop
+    event.preventDefault();
+    const draggableElementData = event.dataTransfer.getData("text/plain");
+    const draggableElement = document.getElementById(draggableElementData);
+    // event.target.insertAdjacentHTML("afterbegin", `<img id="${draggableElementData}" class="draggable" draggable="true" src="img/1.jpeg" alt="Imagen 1">`)
+    //draggableElement.style.cssText = 'max-width: 100px; max-height: 100px; margin-bottom: 20px; padding: 10px 20px; border-radius: 20px;';
+    draggableElement.style.cssText = 'width: 100px; height: 100px; margin-bottom: 5px; padding: 10px 10px; border-radius: 20px;';
+    narrowDiv.appendChild(draggableElement);
+    console.log(draggableElementData); // muestra el id de la imagen el nombre
+    descontarTotal(draggableElement.value)
     //Extraer el precio y mostrarlo en el contenedor
     // const mostrarPrecio = document.querySelector("#contenedor p");
     // mostrarPrecio.appendChild(precio);
-// }
-    
-
 }
 
+let total = 0;
 
+function calcularTotal(precio) {
 
+    limpiarHTML();
+    total += precio;
+    const parrafoTotal = document.createElement('P');
+    parrafoTotal.innerHTML = `TOTAL A PAGAR: $${total}`;
+    parrafoTotal.style.cssText = 'font-weight: bolder; font-size: larger; text-align: left';
+    totalDiv.appendChild(parrafoTotal);
+}
+
+function limpiarHTML(){
+    while(totalDiv.firstChild) {
+        totalDiv.removeChild(totalDiv.firstChild);
+    }
+}
+
+function descontarTotal(precio) {
+
+    limpiarHTML();
+    total -= precio;
+    const parrafoTotal = document.createElement('P');
+    parrafoTotal.innerHTML = `TOTAL A PAGAR: $${total}`;
+    parrafoTotal.style.cssText = 'font-weight: bolder; font-size: larger;';
+    totalDiv.appendChild(parrafoTotal);
+}
+}
